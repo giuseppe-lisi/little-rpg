912818c3a6ec20b4aa11f2bcf4c76cc0c087268f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-14 22:22:00 +0200

    è possibile combattere ora contro uno slime

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import rpg.classes.entityClasses.Player;
 import rpg.classes.entityClasses.enemyTypes.Goblin;
+import rpg.classes.entityClasses.enemyTypes.Slime;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,14 +11,15 @@ public class App {
 
         Player player = new Player();
         Goblin goblin = new Goblin(100, 5, 0.5f);
+        Slime slime = new Slime(30, 5, 0.5f);
 
-        while (goblin.getHp() > 0) {
+        while (slime.getHp() > 0) {
 
             if (playerTurn) {
                 System.out.println("It's the enemy's turn!");
-                goblin.drawSprite();
+                slime.drawSprite();
                 System.out.println("*---------------------*");
-                goblin.getHpString();
+                slime.getHpString();
                 System.out.println("*---------------------*");
                 System.out.println("HP: " + player.getHp() + " | Attack: " + player.getAttack() + " | Defense: "
                         + player.getDefense());
@@ -27,12 +29,12 @@ public class App {
                 // if player doesnt correctly type y to attack he doesnt deal damage and gets
                 // attacked
                 if (choice.equals("y")) {
-                    player.dealDamage(goblin);
+                    player.dealDamage(slime);
                 }
                 playerTurn = !playerTurn;
 
             } else { // goblin deals damage and gives back turn to player
-                goblin.dealDamage(player);
+                slime.dealDamage(player);
                 playerTurn = !playerTurn;
             }
         }
---
+++ src/rpg/classes/entityClasses/enemyTypes/Slime.java
@@ -0,0 +1,59 @@
+package rpg.classes.entityClasses.enemyTypes;
+import rpg.classes.entityClasses.Enemy;
+import rpg.classes.entityClasses.Player;
+
+public class Slime extends Enemy {
+    protected int hp;
+    protected int attack;
+    protected float defense;
+
+    public Slime(int hp, int attack, float defense) {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    // getter setter
+    public int getHp() {
+        return hp;
+    }
+
+    public void getHpString() {
+        System.out.println("Current goblin hp: " + hp);
+    }
+
+    // class utils
+    @Override
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+    }
+
+    @Override
+    public void dealDamage(Player player) {
+        player.takeDamage(attack);
+    }
+    // draws goblin sprite on screen
+    @Override
+    public void drawSprite() {
+        System.err.print("........................................\n" + //
+                        "................................  ......\n" + //
+                        "............... ::. .       .. ::: .....\n" + //
+                        "................ ::::.:++:::::::: ......\n" + //
+                        "............... :.::::::::::::::  ......\n" + //
+                        "............. :++:::::::::::::::: ......\n" + //
+                        "...........  :+:::::::::  ::::- :: .....\n" + //
+                        ".......... :++::::::::=+ :::::..+ : ....\n" + //
+                        "......... ::::::::::::::::::::::::: ....\n" + //
+                        "........ :::::::::::::. ..  :  . :: ....\n" + //
+                        "....... :::::::::::::  .::.   ::  :. ...\n" + //
+                        "...... ::::::::::::::::::::::::::::. ...\n" + //
+                        "....  :::::::::::::::::::::::::::::. ...\n" + //
+                        ".. ::::::::::::::::::::::::::::::::  ...\n" + //
+                        "   ::::::::::::::::::::::::::::::::::  .\n" + //
+                        "....  ::::::::::::::::::::::::::::::: ..\n" + //
+                        "......  :::::::::::::::::::::.:::    ...\n" + //
+                        "...........      .....     .............\n" + //
+                        "........................................\n" + //
+                        "");
+    }
+}
