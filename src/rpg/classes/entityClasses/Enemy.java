49d0daa323b25f2ddff908bae633ca2f0f940214
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-14 23:28:47 +0200

    aggiunti metodi da implementare gethp e getHpString

--- src/rpg/classes/entityClasses/Enemy.java
+++ src/rpg/classes/entityClasses/Enemy.java
@@ -2,9 +2,16 @@ package rpg.classes.entityClasses;
 
 public abstract class Enemy {
 
+    // damage and attack - take damage also works as setter for hp
     public abstract void takeDamage(int damage);
 
     public abstract void dealDamage(Player player);
 
+    // getter hp
+    public abstract int getHp();
+    
+    public abstract void getHpString();
+
+    // draws sprite
     public abstract void drawSprite();
 }
