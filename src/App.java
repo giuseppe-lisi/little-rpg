c611c9163793122730a7e4a03452fa64dd9f58c5
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-14 23:28:59 +0200

    migliorata scrittura e flow del gioco

--- src/App.java
+++ src/App.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 import rpg.classes.entityClasses.Player;
 import rpg.classes.entityClasses.enemyTypes.Goblin;
 import rpg.classes.entityClasses.enemyTypes.Slime;
+import rpg.classes.entityClasses.Enemy;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+
         // player has first turn
         boolean playerTurn = true;
 
@@ -13,37 +15,65 @@ public class App {
         Goblin goblin = new Goblin(100, 5, 0.9f);
         Slime slime = new Slime(30, 5, 0.7f);
 
-        while (goblin.getHp() > 0) {
+        Enemy currentEnemy = slime;
+
+        while (player.getHp() > 0) {
 
             if (playerTurn) {
-                goblin.drawSprite();
-                System.out.println("*---------------------*");
-                System.out.println("The enemy taunts you: " + goblin.taunt());
-                System.out.println("*---------------------*");
-                goblin.getHpString();
+                currentEnemy.drawSprite();
+
+                if (currentEnemy instanceof Goblin) {
+                    // downcasting to allow call to taunt method
+                    currentEnemy = (Goblin) goblin;
+                    System.out.println("*---------------------*");
+                    System.out.println("The enemy taunts you: " + goblin.taunt());
+                    System.out.println("*---------------------*");
+                }
+
+                currentEnemy.getHpString();
                 System.out.println("*---------------------*");
                 System.out.println("HP: " + player.getHp() + " | Attack: " + player.getAttack() + " | Defense: "
-                + player.getDefense());
+                        + player.getDefense());
                 System.out.println("*---------------------*");
                 System.out.println("Attack? (y)");
                 String choice = input.nextLine();
                 // if player doesnt correctly type y to attack he doesnt deal damage and gets
                 // attacked
                 if (choice.equals("y")) {
-                    player.dealDamage(goblin);
+                    player.dealDamage(currentEnemy);
                 }
                 System.out.println("*---------------------*");
                 System.out.println("It's the enemy's turn!");
                 playerTurn = !playerTurn;
-                
-            } else { // goblin deals damage and gives back turn to player
-                System.out.println("*---------------------*");
-                System.out.println("The enemy attacks aswell!");
-                System.out.println("*---------------------*");
-                goblin.dealDamage(player);
-                playerTurn = !playerTurn;
+
+            } else {
+                // currentEnemy deals damage and gives back turn to player
+                if (currentEnemy.getHp() > 0 && player.getHp() > 0) {
+                    System.out.println("*---------------------*");
+                    System.out.println("The enemy attacks aswell!");
+                    System.out.println("*---------------------*");
+                    currentEnemy.dealDamage(player);
+                    playerTurn = !playerTurn;
+                }
             }
+
+            // print game over message if player died otherwise go back and get new enemy
         }
 
+        System.out.println("▗▄▄▄▖▐▌   ▗▞▀▚▖    ▄▄▄▄   ▄▄▄  ▄▄▄▄   ▄▄▄  ■  ▗▞▀▚▖ ▄▄▄ ▄▄▄     ▄   ▄  ▄▄▄  ▄▄▄▄  \n" + //
+                "  █  ▐▌   ▐▛▀▀▘    █ █ █ █   █ █   █ ▀▄▄▗▄▟▙▄▖▐▛▀▀▘█   ▀▄▄      █ ▄ █ █   █ █   █ \n" + //
+                "  █  ▐▛▀▚▖▝▚▄▄▖    █   █ ▀▄▄▄▀ █   █ ▄▄▄▀ ▐▌  ▝▚▄▄▖█   ▄▄▄▀     █▄█▄█ ▀▄▄▄▀ █   █ \n" + //
+                "  █  ▐▌ ▐▌                                ▐▌                                      \n" + //
+                "                                          ▐▌                                      \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  \n" + //
+                "                                                                                  ");
+        input.close();
     }
 }
