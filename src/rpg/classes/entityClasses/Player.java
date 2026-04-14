6201863e1e959c1d749b10b98e9a5349bc3df33a
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-15 01:38:41 +0200

    fatto clamp dell'HP del giocatore e aggiunta la possibilità di usare fino a 3 pozioni per rigenerare la vita

--- src/rpg/classes/entityClasses/Player.java
+++ src/rpg/classes/entityClasses/Player.java
@@ -1,15 +1,20 @@
 package rpg.classes.entityClasses;
 
 public class Player {
-    private int hp = 100;
+    private int maxHp = 100;
+    private int hp = maxHp;
     private int attack = 30;
     private float defense = 0.9f;
     private int killCount = 0;
-
+    private int healingPotions = 3; 
 
     // setter getter
     public int getHp() {
         return hp;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
     }
 
     public int getAttack() {
@@ -34,20 +39,24 @@ public class Player {
     }
 
     public void heal() {
-        hp += 50;
+        healingPotions -= 1;
+        if (healingPotions > 0) {
+            hp = Math.clamp(hp + 50, 0, maxHp);
+        }
     }
     
     // stats methods
     public void getStats() {
-        System.out.println("*---------------------*");
-        System.out.println("HP: " + hp + " | Attack: " + attack + String.format(" | Defense: %.2f", defense));
-        System.out.println("*---------------------*");
+        System.out.println("*-------------------------------------------------*");
+        System.out.println("HP: " + hp + " / " + maxHp + " | Attack: " + attack);
+        System.out.println("Potions: " + healingPotions + String.format("    | Defense: %.2f", defense));
+        System.out.println("*-------------------------------------------------*");
     }
 
     public void lvlUpStats() {
         if (killCount % 3 == 0) {
             System.out.println("You leveled up!");
-            hp += 20;
+            maxHp += 20;
             attack += 10;
             defense -= 0.1;
         }
